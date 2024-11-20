bc8f198ce7879463593405f8e046ec073a6fca54
Ysen0603 <yassineennaya2264@gmail.com>
2024-11-20 15:59:27 +0100

    "fixing the cros error"

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://yassineennaya2264:
 
 // Configuration CORS
 app.use(cors({
-  origin: ['https://ada-frontend-icrm801vg-ennaya-yassines-projects.vercel.app', 'https://ada-backend-p8o0.onrender.com'],
+  origin: ['https://ada-frontend.vercel.app', 'https://ada-backend-p8o0.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
